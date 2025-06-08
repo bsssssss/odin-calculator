2d252fadf6ed0626dba5f8f7262308ce8089e119
Bss <mx.bss@proton.me>
2025-06-08 21:42:37 +0200

    buggy hover

--- scripts.js
+++ scripts.js
@@ -1,12 +1,23 @@
 const calculator = document.querySelector('.calculator');
 const displayText = document.querySelector('.display-text');
-const buttons = document.querySelectorAll('.buttons-container');
+const buttons = document.querySelectorAll('button');
 const resetBtn = document.querySelector('.reset');
 const numpad = document.querySelector('.numpad');
 const operators = document.querySelector('.operators');
 
 function initCalculator() {
     displayText.textContent = 'Mathematics strikes back.';
+}
+
+for (let button of buttons) {
+    button.addEventListener('mouseover', (e) => {
+        console.log(`hovering ${e.target}`);
+        button.style.opacity = 0.5;
+    });
+    button.addEventListener('mouseout', (e) => {
+        console.log(`leaving ${e.target}`);
+        button.style.opacity = 1;
+    });
 }
 
 window.onload = initCalculator();
