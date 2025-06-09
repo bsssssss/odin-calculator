9f781604b514402f67988542c1c70c4f7f179461
Bss <mx.bss@proton.me>
2025-06-09 21:18:55 +0200

    It is alive ! (i think)

--- scripts.js
+++ scripts.js
@@ -13,6 +13,8 @@ function mult(x, y) {
 
 function operate(op, x, y) {
     let result = 0;
+    x = parseFloat(x);
+    y = parseFloat(y);
     if (op == '+') {
         result = add(x, y);
     }
@@ -28,6 +30,12 @@ function operate(op, x, y) {
     return result;
 }
 
+let leftOperand = '';
+let rightOperand = '';
+let operator = '';
+let isSetLeftOperand = false;
+let isSetRightOperand = false;
+
 const displayText = document.querySelector('.display-text');
 function updateDisplay(string) {
     displayText.textContent = string;
@@ -35,27 +43,68 @@ function updateDisplay(string) {
 
 const resetButton = document.querySelector('.reset-button');
 resetButton.addEventListener('click', () => {
-    console.log('Reset !');
+    leftOperand = '';
+    rightOperand = '';
+    operator = '';
+    isSetLeftOperand = false;
+    isSetRightOperand = false;
     updateDisplay('');
 });
 
 const numberButtons = document.querySelectorAll('.number-button');
 for (let button of numberButtons) {
     button.addEventListener('click', () => {
-        console.log(`${button.textContent}`);
+        let number = button.textContent;
+
+        if (!isSetLeftOperand) {
+            console.log(`left operand not set, adding number to left operand`);
+            leftOperand += number;
+            updateDisplay(leftOperand);
+        } else {
+            console.log(`left operand set, adding number to right operand`);
+            rightOperand += number;
+            updateDisplay(rightOperand);
+        }
     });
 }
 
 const operatorButtons = document.querySelectorAll('.operator-button');
 for (let button of operatorButtons) {
     button.addEventListener('click', () => {
-        console.log(`${button.textContent}`);
+        if (leftOperand.length > 0) {
+            console.log(`left operand is a number`);
+
+            if (!isSetLeftOperand) {
+                console.log(
+                    `left operand is not set, setting it to true now !`
+                );
+                isSetLeftOperand = true;
+            } else if (rightOperand.length > 0) {
+                console.log(
+                    `left operand is set and right operand is a number\n\tcalculating...`
+                );
+
+                let result = operate(operator, leftOperand, rightOperand);
+                leftOperand = result.toString();
+                rightOperand = '';
+
+                updateDisplay(leftOperand);
+            }
+        }
+        operator = button.textContent;
     });
 }
 
 const calculateButton = document.querySelector('.calculate-button');
 calculateButton.addEventListener('click', () => {
-    console.log(`Calculate !`);
+    if (isSetLeftOperand && operator.length > 0) {
+        let result = operate(operator, leftOperand, rightOperand);
+
+        leftOperand = result.toString();
+        rightOperand = '';
+
+        updateDisplay(leftOperand.toString());
+    }
 });
 
 function initCalculator() {
