eea8589ac61f67ef473c4e7e22866fe95d7fd45d
Bss <mx.bss@proton.me>
2025-06-09 19:41:08 +0200

    add clicks event listeners

--- scripts.js
+++ scripts.js
@@ -1,62 +1,18 @@
-const displayText = document.querySelector('.display-text');
-const buttons = document.querySelectorAll('button');
-
-let firstNumber = '';
-let secondNumber = '';
-let operator = '';
-
-function initCalculator() {
-    for (let button of buttons) {
-        button.addEventListener('click', (e) => {
-            handleClicks(button, e);
-        });
-    }
-    updateDisplay('Mathematics strikes back.');
-}
-
-function handleClicks(button, event) {
-    if (displayText.textContent != '') {
-        updateDisplay('');
-    }
-}
-
-function isNumber(button) {
-    return button.classList.value.includes('number');
-}
-
-function isReset(button) {
-    return button.classList.value.includes('reset');
-}
-
-function isOperator(button) {
-    return button.classList.value.includes('operator');
-}
-
-function updateDisplay(string) {
-    displayText.textContent = string;
-}
-
-window.onload = initCalculator();
-
 function add(x, y) {
     return x + y;
 }
-
 function subtract(x, y) {
     return x - y;
 }
-
 function divide(x, y) {
     return x / y;
 }
-
 function mult(x, y) {
     return x * y;
 }
 
 function operate(op, x, y) {
     let result = 0;
-
     if (op == '+') {
         result = add(x, y);
     }
@@ -69,6 +25,40 @@ function operate(op, x, y) {
     if (op == '*') {
         result = mult(x, y);
     }
-
     return result;
 }
+
+const displayText = document.querySelector('.display-text');
+function updateDisplay(string) {
+    displayText.textContent = string;
+}
+
+function initCalculator() {
+    updateDisplay('Mathematics strikes back.');
+}
+window.onload = initCalculator();
+
+const resetButton = document.querySelector('.reset-button');
+resetButton.addEventListener('click', () => {
+    console.log('Reset !');
+    updateDisplay('');
+});
+
+const numberButtons = document.querySelectorAll('.number-button');
+for (let button of numberButtons) {
+    button.addEventListener('click', () => {
+        console.log(`${button.textContent}`);
+    });
+}
+
+const operatorButtons = document.querySelectorAll('.operator-button');
+for (let button of operatorButtons) {
+    button.addEventListener('click', () => {
+        console.log(`${button.textContent}`);
+    });
+}
+
+const calculateButton = document.querySelector('.calculate-button');
+calculateButton.addEventListener('click', () => {
+    console.log(`Calculate !`);
+});
